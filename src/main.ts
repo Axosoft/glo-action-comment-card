4030fdd9794a689226711783e792fb941ec233f9
Dan Suceava <dans@axosoft.com>
2019-08-05 16:24:35 -0700

    add action code

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import * as core from '@actions/core';
+import GloSDK from '@axosoft/glo-sdk';
 
 async function run() {
+  const authToken = core.getInput('authToken');
+  const boardID = core.getInput('boardID');
+  const cardID = core.getInput('cardID');
+  const comment = core.getInput('comment');
+
   try {
-    const myInput = core.getInput('myInput');
-    core.debug(`Hello ${myInput}`);
+    // add the comment
+    await GloSDK(authToken).boards.cards.comments.create(boardID, cardID, {
+      text: comment
+    });
   } catch (error) {
     core.setFailed(error.message);
   }
